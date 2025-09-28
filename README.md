ec089c902cdfd2b3ac74f78f85466e73eb10fd98
Anton Grabovsky <agrabovsky2020@gmail.com>
2025-09-28 23:01:02 +0300

    feat: Update script to generate keymaps.lua

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 **Linux**:
 
-```sh
+```bash
 make install
 # or
 make
@@ -14,7 +14,7 @@ make
 
 **Linux**:
 
-```sh
+```bash
 make delete
 # or delete configuration along with backups
 make delete DELETE_WITH_BACKUPS=true
@@ -28,9 +28,23 @@ You need to install: rust-analyzer, rustfmt, cargo-nextest.
 
 **Linux**:
 
-```sh
+```bash
 rustup component add rust-analyzer
 rustup component add rustfmt
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall cargo-nextest --secure
 ```
+
+## Replaced [keymaps](https://www.lazyvim.org/keymaps)
+
+### General
+
+| № | Action  | Default Key  | Custom Key   | Description         | Mode        |
+|---|---------|--------------|--------------|---------------------|-------------|
+| 1 | Replace | `<A-j>`      | `<A-Down>`   | Move Down           | **n, i, v** |
+| 2 | Replace | `<A-k>`      | `<A-Up>`     | Move Up             | **n, i, v** |
+| 3 | Add     |              | `<C-/>`      | Toggle comment line | **n, v**    |
+| 4 | Replace | `<c-/>`      | `<C-`>`      | Terminal (Root Dir) | **n**       |
+| 5 | Replace | `<C-/>`      | `<C-`>`      | Hide Terminal       | **t**       |
+| 6 | Replace | `<leader>-`  | `<leader>\|` | Split Window Below  | **n**       |
+| 7 | Replace | `<leader>\|` | `<leader>\`  | Split Window Right  | **n**       |
--- scripts/generate_lua_files.sh
+++ scripts/generate_lua_files.sh
@@ -79,3 +79,47 @@ config_neotest_file="require('neotest').setup {
     }
 }"
 echo "$config_neotest_file" > ~/.config/nvim/lua/config/plugins/neotest.lua
+
+read -r -d '' keymaps_file << 'EOM'
+-- https://neovim.io/doc/user/map.html#%3Amap-verbose
+-- listing a key map will also display where it was last defined
+-- nvim -V1, :verbose map or :verbose map <A-Down>
+
+local set = vim.keymap.set
+local del = vim.keymap.del
+
+-- Override keymaps
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+
+-- Move Lines
+del("n", "<A-j>")
+set("n", "<A-Down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+del("n", "<A-k>")
+set("n", "<A-Up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+del("i", "<A-j>")
+set("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+del("i", "<A-k>")
+set("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+del("v", "<A-j>")
+set("v", "<A-Down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+del("v", "<A-k>")
+set("v", "<A-Up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+-- Comments
+set("n", "<C-/>", "gcc", { desc = "Toggle comment line", remap = true })
+set("v", "<C-/>", "gc", { desc = "Toggle comment line", remap = true })
+
+-- floating terminal
+set("n", "<C-`>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+
+-- Terminal Mappings
+set("t", "<C-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+-- windows
+del("n", "<leader>-")
+set("n", "<leader>|", "<C-W>s", { desc = "Split Window Below", remap = true })
+set("n", "<leader>\\", "<C-W>v", { desc = "Split Window Right", remap = true })
+EOM
+echo "$keymaps_file" > ~/.config/nvim/lua/config/keymaps.lua
